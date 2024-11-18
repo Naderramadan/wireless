c0818746eceec8f202f0acc913ef769f9835c5ca
Naderramadan <78705819+Naderramadan@users.noreply.github.com>
2024-11-17 21:48:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
-// Hamburger Menu Toggle for Mobile Devices
-const hamburgerMenu = document.getElementById("hamburger-menu");
-const navLinks = document.querySelector(".nav-links");
-
-hamburgerMenu.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+// Script for "Back to Top" Button functionality
+document.querySelector('.back-to-top').addEventListener('click', function (e) {
+    e.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
